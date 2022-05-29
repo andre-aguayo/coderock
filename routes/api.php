5820212fbc70c36e694507def61630b0dc768e4f
andre-aguayo <andre.aguayo@hotmail.com>
2022-05-29 18:05:06 -0400

    criaçao das rotas

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\InvestmentController;
+use App\Http\Controllers\InvestorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+/**
+ * Como nao há necessidade de autenticaçao para o teste todas as rotas srao definidas fora do middleware 'auth:sanctum'
+ * 
+ */
+
+Route::prefix('investor')->group(function () {
+    Route::get('/{id}', [InvestorController::class, 'show']);
+    Route::post('/store', [InvestorController::class, 'store']);
+    Route::get('/{id}/investments', [InvestorController::class, 'showInvestments']);
+});
+
+Route::prefix('investment')->group(function () {
+    Route::get('/{id}', [InvestmentController::class, 'show']);
+    Route::post('/store', [InvestmentController::class, 'store']);
+    Route::get('/{id}/{projectionDate}', [InvestmentController::class, 'showInvestmentProjection']);
+    Route::post('/withdraw/{id}/{withdrawDate}',[InvestmentController::class,'withdraw']);
 });
